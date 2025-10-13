f5957db6a158650406112b95d97acb1a84b9abfa
Devoura Studio <linkedinmiran@gmail.com>
2025-10-13 21:46:41 +0330

    Select elements using DOM in js.

---
+++ index.js
@@ -0,0 +1,6 @@
+const spanOne = document.querySelector('#Span-1')
+const spanTwo = document.querySelector('#Span-2')
+const buttonOne = document.querySelector('#Button-1')
+const buttonTwo = document.querySelector('#Button-2')
+const buttonThree = document.querySelector('#Button-3')
+const select = document.querySelector('select')
