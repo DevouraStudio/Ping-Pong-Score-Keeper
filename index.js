c8e1126f43cf58d6fe2bce0bed8c4adc21530da0
Devoura Studio <linkedinmiran@gmail.com>
2025-10-15 19:38:13 +0330

    Complete the main content.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const spanOne = document.querySelector('#Span-1')
+
 const spanTwo = document.querySelector('#Span-2')
+
 const buttonOne = document.querySelector('#Button-1')
+
 const buttonTwo = document.querySelector('#Button-2')
+
 const buttonThree = document.querySelector('#Button-3')
+
 const select = document.querySelector('select')
+
 let score = 0
+
 let score2 = 0
+
 let maxScore = parseInt(select.value)
+
 let gameOver = false
+
 buttonOne.addEventListener('click', function () {
 	maxScore = parseInt(select.value)
 	if (!gameOver) {
@@ -24,9 +34,6 @@ buttonOne.addEventListener('click', function () {
 	spanOne.textContent = score
 }
 )
-
-
-
 
 buttonTwo.addEventListener('click', function () {
 	maxScore = parseInt(select.value)
