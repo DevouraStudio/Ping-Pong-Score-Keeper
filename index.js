10e3c3236bc75a4a5459409851777251139c8add
Devoura Studio <linkedinmiran@gmail.com>
2025-10-15 15:58:21 +0330

    Configure variables.

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const buttonOne = document.querySelector('#Button-1')
 const buttonTwo = document.querySelector('#Button-2')
 const buttonThree = document.querySelector('#Button-3')
 const select = document.querySelector('select')
+let score = 0
+let maxScore = select.value
